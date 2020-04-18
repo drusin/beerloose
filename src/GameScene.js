649520e7cef6a91a3b0d32c65254d79fed4df3da
Simon Lenz <post@simon-lenz.de>
2020-04-19 00:38:47 +0200

    make player moveable

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,12 +1,14 @@
 import { Scene } from 'phaser';
 import BeerBar from './beer_bar';
-import { discoDancer, squaredancer, metalDancer, player, preloadAllSprites, createAnimationsForAllSprites } from './sprites';
+import { discoDancer, squaredancer, metalDancer, preloadAllSprites, createAnimationsForAllSprites } from './sprites';
+import { createPlayer } from './entities/player.js'
 
 export default class GameScene extends Scene {
 	constructor() {
 		super({ key: GameScene.KEY });
 		this.keys = undefined;
 		this._count = 0;
+		this.player = createPlayer();
 	}
 	
 	static get KEY() {
@@ -18,24 +20,22 @@ export default class GameScene extends Scene {
 	}
 
 	create() {
+		createAnimationsForAllSprites({ scene: this });
 		this.keys = this.input.keyboard.addKeys("W,A,S,D,LEFT,UP,RIGHT,DOWN,SPACE,ENTER");
 		const { width, height } = this.sys.game.canvas;
+
+		this.player.create({ scene: this, x: 300, y: 100 });
 
 		this._drops = this.physics.add.group();
 		this._player = this.physics.add.image(500, 600);
 		this._player.setInteractive();
 		
-		createAnimationsForAllSprites({ scene: this });
-
 		const discoDude = discoDancer.create({ scene: this, x: 100, y: 100 });
 		discoDude.anims.play('disco-dancer-up-down', true);
 		const squareGal = squaredancer.create({ scene: this, x: 150, y: 100 });
 		squareGal.anims.play('squaredancer-left-right', true);
 		const metaller = metalDancer.create({ scene: this, x: 200, y: 100 });
 		metaller.anims.play('metal-dancer-headbang', true);
-
-		const playerC = player.create({ scene: this, x: 100, y: 150, sheet: 'player-two-beers-spritesheet' });
-		playerC.anims.play('two_beers-player-drop', true);
 
 		this.physics.add.overlap(this._player, this._drops,
 			(...args) => args.filter(arg => arg !== this._player).forEach(arg => arg.destroy())
@@ -58,5 +58,7 @@ export default class GameScene extends Scene {
 			this.beer.decrease(1);
 			this._count -= 800;
 		}
+
+		this.player.updateMovement({ keys: this.keys });
 	}
 }
---
+++ src/entities/player.js
@@ -0,0 +1,42 @@
+import { player as playerSprite } from '../sprites';
+
+const PLAYER_SPEED = 100;
+
+export function createPlayer() {
+    let sprite;
+    return {
+        sprite,
+        create: function({ scene, x, y }) {
+            sprite = playerSprite.create({ scene, x, y, sheet: 'player-two-beers-spritesheet' });
+		    sprite.anims.play('two_beers-player-drop', true);
+            sprite.setInteractive();
+        },
+        updateMovement: function({ keys }) {
+            const { LEFT, RIGHT, UP, DOWN, W, A, S, D } = keys;
+        
+            const leftPressed = (LEFT.isDown || A.isDown);
+            const rightPressed = (RIGHT.isDown || D.isDown);
+            const upPressed = (UP.isDown || W.isDown);
+            const downPressed = (DOWN.isDown || S.isDown);
+        
+            const direction = { x: 0, y: 0 };
+            if (leftPressed) { direction.x = -1; }
+            else if (rightPressed) { direction.x = 1; }
+            if (upPressed) { direction.y = -1; }
+            else if (downPressed) { direction.y = 1; }
+            
+            const normalizedDirection = normalize2dVector(direction);
+            sprite.setVelocityX(PLAYER_SPEED * normalizedDirection.x);
+            sprite.setVelocityY(PLAYER_SPEED * normalizedDirection.y);
+        },
+    };
+}
+
+function normalize2dVector(vector) {
+    if (vector.x === 0 && vector.y === 0) return vector;
+    const norm = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
+    return {
+        x: vector.x / norm,
+        y: vector.y / norm,
+    };
+}
