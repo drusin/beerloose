0d6ea0987c3a6fc428a3ab75ff3b3fa963f249b6
svenhemmer <sven.hemmer@googlemail.com>
2020-04-19 23:41:20 +0200

    Decrease happiness per second

--- src/entities/gal.js
+++ src/entities/gal.js
@@ -5,6 +5,13 @@ const INDICATOR_OFFSET = 18 * SPRITE_SCALE_FACTOR;
 export function createGal({ type }) {
     let happiness = 100;
 
+    const indicators = {
+        'indicator-good': [100, 80],
+        'indicator-medium': [79, 30],
+        'indicator-bad': [29, 1],
+        'indicator-leaving': [0, 0]
+    };
+
     return {
         sprite: {},
         createSprite: function({ scene, x, y }) {
@@ -12,6 +19,7 @@ export function createGal({ type }) {
             this.indicatorSprite.anims.play('indicator-good', true);
             this.indicatorSprite.setDepth(1000 + y);
             this.sprite = gal.create({ scene, x, y, number: type });
+            this.delta_counter = 0;
         },
         updateMovement() {
             // no movement
@@ -25,9 +33,26 @@ export function createGal({ type }) {
                 }
             );
         },
-        updateAnimation(delta) {
+        updateAnimation({ delta }) {
+            this.delta_counter += delta;
+            if (this.delta_counter > 1000 && happiness) {
+                this.delta_counter = 0;
+                happiness--;
+                console.log(happiness);
+                this.indicatorSprite.anims.play(getMoodIndicator(), true);
+            }
             this.sprite.anims.play(`${type}-gal-hair`, true).setOrigin(0, 0);
             this.sprite.setDepth(this.sprite.y);
         }
    };
+
+   function getMoodIndicator() {
+        let indicator = '';
+        Object.entries(indicators).forEach(([name, boundaries]) => {   
+           if (boundaries[0] >= happiness && boundaries[1] <= happiness) {
+               indicator = name;
+           }
+       });
+       return indicator;
+   }
 }
