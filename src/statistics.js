33776cb6569f949fe8489406e03cba5335560a40
Simon Lenz <post@simon-lenz.de>
2020-04-20 23:22:06 +0200

    swithc statistics server

--- src/statistics.js
+++ src/statistics.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://rusin.xyz:5555";
+const SERVER_URL = "https://thumbnails.simon-lenz.de";
 
 export async function sendStatistics({ username, score }) {
     const content = {
