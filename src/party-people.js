d30770d78be8333551baeae728641566de4d8e05
svenhemmer <sven.hemmer@googlemail.com>
2020-04-20 13:17:09 +0200

    Spawn dancers at 30 fixed spots randomly every 20 seconds

--- src/party-people.js
+++ src/party-people.js
@@ -3,23 +3,53 @@ import {
     createMetalDancer, 
     createSquaredanceDancer
 } from './entities';
-import { generateRandomPositionOnDanceFloor } from './util.js';
+
+const spots = [];
+const MILLIS_TILL_SPAWN = 20000;
+
+function initSpots(rows, cols, xOffset = 150, yOffset = 80, xDist = 60, yDist = 40) {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            spots.push({
+                x: xOffset + c * xDist,
+                y: yOffset + r * yDist,
+            })
+        }
+    }
+}
+
+function getRandomSpot() {
+    const index = Math.floor(Math.random() * spots.length);
+    return spots.splice(index, 1)[0];
+}
 
 export function createPartyPeople() {
     return {
         partyPeople: [],
-        initialize: function ({ scene, dancerCount = 20 }) {
+        initialize: function ({ scene, dancerCount = 4 }) {
+            initSpots(5, 6);
+            this.current_delta = 0;
+            this.scene = scene;
             for (let i = 0; i < dancerCount; i++) {
-                const dancer = generateRandomDancer();
-                dancer.createSprite({
-                    scene,
-                    x: (150 + (i%5) * 70),
-                    y: (100 + Math.floor(i/5) * 50) 
-                });
-                this.partyPeople.push(dancer);
+                this.spawnDancer();
             }
         },
+        spawnDancer() {
+            const dancer = generateRandomDancer();
+            const spot = getRandomSpot();
+            dancer.createSprite({
+                scene: this.scene,
+                x: spot.x,
+                y: spot.y 
+            });
+            this.partyPeople.push(dancer);
+        },
         update: function (delta) {
+            this.current_delta += delta;
+            if (spots.length && this.current_delta > MILLIS_TILL_SPAWN) {
+                this.current_delta = 0;
+                this.spawnDancer();
+            }
             this.partyPeople.forEach(person => person.updateMovement());
             this.partyPeople.forEach(person => person.updateAnimation({ delta }));
         },
