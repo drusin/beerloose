e005fe9a011850c337fd24323094de80f2e710f9
Murat Özkan <smuratozkan@gmail.com>
2020-04-20 20:54:31 +0200

    The mood slider working fine now

--- src/mood_slider.js
+++ src/mood_slider.js
@@ -39,40 +39,25 @@ export default class MoodSlider {
         this.badSmiley = this.graphics.scene.add.sprite(this.x - 35, this.y + 27, 'mood_smileys');
         this.badSmiley.setScale(0.6);
 
-        this.moodPositiveSprite = this.graphics.scene.add.image(this.x + 128, this.y + 27, 'mood_positive');
+        this.moodPositiveSprite = this.graphics.scene.add.image(this.x + 126, this.y + 27, 'mood_positive');
         this.moodPositiveSprite.setScale(0.5);
-        console.log(this.moodPositiveSprite);
 
         this.moodNegativeSprite = this.graphics.scene.add.image(this.x + 36, this.y + 27, 'mood_negative');
-        
         this.moodNegativeSprite.setScale(0.5);
     }
 
     draw() {
-        // const value = this.women.getTotalHappiness(); 
+        this.graphics.clear();
 
-        const value = 85;
+        const value = this.women.getTotalHappiness(); 
 
-        const xOffs = this.moodPositiveSprite.width * Math.max(0, (value - 50)/50);
-        
-        // this.moodPositiveSprite.scaleX = Math.max(0, (value - 50)/50);
-        // this.moodPositiveSprite.setPosition(this.x + 128 - xOffs, this.y + 27);
-        // const cropArea = new Geom.Rectangle(0, 0, xOffs , this.moodPositiveSprite.height);
-        // this.moodPositiveSprite.flipX = true;
-        // this.moodPositiveSprite.setCrop(cropArea);
-        // if (value > 50) {
-        // }
+        // 100 -> 0.0, 50 -> 1.0
+        const xOffs = this.moodPositiveSprite.width *  Math.max(0, (100 - value)/50); // Math.max(0, (value - 50)/50);
+        const posCrop = new Geom.Rectangle(xOffs, 0, this.moodPositiveSprite.width, this.moodPositiveSprite.height);
+        this.moodPositiveSprite.setCrop(posCrop).flipX = true;
 
-        const cropArea2 = new Geom.Rectangle(0, 0, this.moodNegativeSprite.width * Math.min(1, (50 - value)/50), this.moodNegativeSprite.height);
-        this.moodNegativeSprite.setCrop(cropArea2).flipX = true;
-
-
-        // this.bar.clear();
-
-        // // we have 34 sprites in 'beer_bar', 0 is full and 35 is empty. mapping amount (0 .. 100) to sprite frame (33 .. 0)
-        // const frame = Math.floor((1 - this.beer.amount / 100) * 33);
-        // this.barSprite.setFrame(frame);
-        // this.beerAmountText.setText('Beer: %' + this.beer.amount);
+        const negCrop = new Geom.Rectangle(0, 0, this.moodNegativeSprite.width * Math.min(1, (50 - value)/50), this.moodNegativeSprite.height);
+        this.moodNegativeSprite.setCrop(negCrop).flipX = true;
     }
 
 }
